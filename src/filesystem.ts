d8ee683ec90586683d9099a158b45d982b2aaf5b
Chandan Kumar <chandan.kumar@niveussolutions.com>
2022-03-27 02:57:19 +0530

    Filesystem updated

--- src/filesystem.ts
+++ src/filesystem.ts
@@ -1,4 +1,4 @@
-import fs from "fs";
+import fs, { fdatasync } from "fs";
 import path from "path";
 
 let file = path.join(__dirname, "..", "a.txt");
@@ -50,5 +50,56 @@ fileStats.isDirectory(); //false
 fileStats.isSymbolicLink(); //false
 fileStats.size; //1024000 //= 1MB
 
+// Reading a file
+// Read the full contents of the file in memory. All the contents are stored in primary memory while executing the code.
+// This is not recommended for large files. Instead use Streams.
+fs.readFile(file, "utf8", (err, data: string) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
+
+// Read the file synchronously
+let data: string = fs.readFileSync(file, "utf8");
+console.log(data);
+
+// Writing a file
+
+let content: string = "Hello world from filesystem";
+// Write the contents of a string to a file.
+fs.writeFile(file, content, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  //file written successfully
+});
+
+// Write the contents of a string to a file synchronously
+try {
+  fs.writeFileSync(file, content);
+  // file written successfully
+} catch (err) {}
+
+// By default this API replaces the file if it already exists.
+// We can override this behavior using flags. supports all flags.
+fs.writeFile(file, content, { flag: "a+" }, (err) => {});
 
 
+// Append to a file
+// Append the contents of a string to a file.
+fs.appendFile(file, content, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  // Append successful
+})
+
+
+// Cons: 
+// After writing all the contents in file. control goes back the main program
+// When a file is read all the contents are stored in primary memory(RAM). This is not recommended for large files.
+// Instead use Streams.
